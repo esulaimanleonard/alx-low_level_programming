88072b78340a36b7b87418f31c1f2346e7bb3481
esulaimanleonard <111577374+esulaimanleonard@users.noreply.github.com>
2022-09-13 07:20:26 +0200

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "leo.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *sh = "_putcher";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
+return (0);
