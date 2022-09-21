f53da57dc0a1d7d8f5d3b9323927856e5a2a4c14
esulaimanleonard <111577374+esulaimanleonard@users.noreply.github.com>
2022-09-21 18:44:55 +0200

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,11 @@
-int _putchar(char);
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
-char *_strcat(char*, char*);
-char *_strncat(char*, char*, int);
-char *_strncpy(char*, char*, int);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
 char *leet(char *);
-int _strcmp(char*, char*);
-void reverse_array(int*, int);
-char *string_toupper(char*);
-char *cap_string(char*);
-char *rot13(char*);
-void print_number(int n)
+char *rot13(char *);
+void print_number(int n);
